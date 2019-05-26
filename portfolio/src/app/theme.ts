8b513c55efb29d5109499fcf6eae2a816602824b
Evan Irla <ageekstouch@gmail.com>
2019-05-25 21:07:25 -0600

    Adding theming and link styling

---
+++ portfolio/src/app/theme.ts
@@ -0,0 +1,9 @@
+export class Theme {
+	label: string;
+    className: string;
+
+    constructor(label: string, className: string) {
+        this.label = label;
+        this.className = className;
+    }
+}
