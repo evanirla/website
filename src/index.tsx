6ec7c8f427d55b814a1d89313a88de5e4d4d4f76
Evan Irla <ageekstouch@gmail.com>
2024-03-09 21:43:34 -0700

    commented out dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -42,43 +42,43 @@ const lightTheme = createTheme({
   }
 });
 
-const darkTheme = createTheme({
-  palette: {
-    background: {
-      default: "black"
-    }
-  },
-  components: {
-    MuiLink: {
-      styleOverrides: {
-        root: {
-          textDecorationColor: "#00FF00"
-        }
-      }
-    },
-    MuiIconButton: {
-      styleOverrides: {
-        root: {
-          color: "#00FF00"
-        }
-      }
-    },
-    MuiDivider: {
-      styleOverrides: {
-        root: {
-          borderColor: "#00FF00" 
-        }
-      }
-    }
-  },
-  typography: {
-    fontFamily: "Inconsolata",
-    fontSize: 16,
-    allVariants: {
-      color: "#00FF00"
-    }
-  }
-});
+// const darkTheme = createTheme({
+//   palette: {
+//     background: {
+//       default: "black"
+//     }
+//   },
+//   components: {
+//     MuiLink: {
+//       styleOverrides: {
+//         root: {
+//           textDecorationColor: "#00FF00"
+//         }
+//       }
+//     },
+//     MuiIconButton: {
+//       styleOverrides: {
+//         root: {
+//           color: "#00FF00"
+//         }
+//       }
+//     },
+//     MuiDivider: {
+//       styleOverrides: {
+//         root: {
+//           borderColor: "#00FF00" 
+//         }
+//       }
+//     }
+//   },
+//   typography: {
+//     fontFamily: "Inconsolata",
+//     fontSize: 16,
+//     allVariants: {
+//       color: "#00FF00"
+//     }
+//   }
+// });
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
