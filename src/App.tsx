7523f2abf798034c91120da8319a0fc65f89eed0
Evan Irla <ageekstouch@gmail.com>
2025-11-04 06:45:58 -0700

    Added work experience and hid interests

--- src/App.tsx
+++ src/App.tsx
@@ -89,10 +89,10 @@ function App() {
             </ul>
           </Typography>
         </section>
-        <section>
+        {/*<section>
           <Typography variant='h5'>Interests</Typography>
           <Typography>
-          <small>These are the skills that I'd like to keep improving.</small>
+            <small>These are the skills that I'd like to keep improving.</small>
             <ul>
               <li>System Architecture</li>
               <li>Requirements Analysis and Design</li>
@@ -101,6 +101,67 @@ function App() {
               <li>CI/CD Integration</li>
               <li>Data Analysis</li>
             </ul>
+          </Typography>
+        </section>*/}
+        <section>
+          <Typography variant='h5'>Experience</Typography>
+          <Typography>
+            <small>These are the projects I've been a part of in the last couple of years, in reverse chronological order.</small>
+          </Typography>
+          <Typography variant='h6'>Code Name: KitchenSink [Jul 2025 - Present]</Typography>
+          <Typography>
+            <p>
+              Planned and organized an upgrade to our base COTS products to the next maintainance release of
+              the company's software platform, as well as bringing in enhancements and bug fixes that were implemented by
+              a number of external teams. This involved defining a detailed project plan with estimates and defining
+              project risks with mitigation strategies. After project kick-off, my role was largely focused on leading
+              the design and development effort and facilitating coordination between my team and the other teams
+              involved to ensure that timelines and quality standards were being met.
+            </p>
+          </Typography>
+          <Typography variant='h6'>Product Support Rotation [May 2025 - Jul 2025]</Typography>
+          <Typography>
+            <p>
+              Spent a few months on a support rotation working with the product support team to help triage and resolve
+              reported issues. This involved many meeting with clients and company staff to determine the root cause of issues
+              across our entire COTS product suite.
+            </p>
+          </Typography>
+          <Typography variant='h6'>Code Name: Skybridge [Mar 2025 - May 2025]</Typography>
+          <Typography>
+            <p>
+              Tech Stack: .NET Framework (C#), .NET 8, OpenText Brava/Blazon
+            </p>
+            <p>
+              Led the technical planning of moving one of our SaaS products to new Azure infrastructure to improve service reliability.
+              This involved performing the migration in a staging environment, identifying and resolving issues, and then creating a detailed migration plan for the production environment.
+              I was also heavily involved in post-migration monitoring and issue resolution to ensure a smooth transition with minimal downtime for our users.
+            </p>
+            <p>
+              The latter half of this project overlapped with my product support rotation, where I was able to leverage my experience with
+              our SaaS offering to quickly address any issues that popped up.
+            </p>
+          </Typography>
+          <Typography variant='h6'>Code Name: GreatValue [Oct 2024 - Mar 2025]</Typography>
+          <Typography>
+            <p>
+              I helped ramp-up a new technical lead on our team before playing a supporting role as lead developer over a team of 3.
+              Halfway through the project, I had to take over the technical lead responsibilities for a month due to unforeseen circumstances.
+              This partially involved a pivot in the project plan to crunch the timeline to meet a hard deadline. 
+            </p>
+          </Typography>
+          <Typography variant='h6'>Code Name: Lasso [Jan 2024 - Oct 2024]</Typography>
+          <Typography>
+            <p>
+              Design System: Figma + Calcite
+            </p>
+            <p>
+              Tech Stack: .NET (C#), React + TypeScript + Calcite, Esri ArcGIS Maps SDK for JavaScript, Oracle Database, Python
+            </p>
+            <p>
+              I served as the lead UX designer and developer on a team of 6, building a new map-centric COTS product for managing inspections/investigations across
+              municipal or state level governments.
+            </p>
           </Typography>
         </section>
         <section>
